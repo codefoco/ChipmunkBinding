2ac4ab94cc89f3a9cdc4aef38c9400804a027c2c
Vinicius Jarina <viniciusjarina@gmail.com>
2019-04-29 18:22:34 -0300

    Added operator/Equals to `cpVect`

--- src/Structs/cpVect.cs
+++ src/Structs/cpVect.cs
@@ -1,15 +1,50 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 #pragma warning disable IDE1006
 
 namespace ChipmunkBinding
 {
     [StructLayout(LayoutKind.Sequential)]
-    public struct cpVect
+    public struct cpVect : IEqualityComparer<cpVect>
     {
         public float x;
         public float y;
+
+        public bool Equals(cpVect x, cpVect y)
+        {
+            return x == y;
+        }
+
+        public int GetHashCode(cpVect obj)
+        {
+            return (obj.x.GetHashCode() << 16) ^ obj.y.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            cpVect? vect = obj as cpVect?;
+            if (!vect.HasValue)
+                return false;
+            return Equals(this, vect.Value);
+        }
+
+        public override int GetHashCode()
+        {
+            return GetHashCode(this);
+        }
+
+        public static bool operator == (cpVect a, cpVect b)
+        {
+            return System.Math.Abs(a.x - b.x) < float.Epsilon && System.Math.Abs(a.x - b.x) < float.Epsilon;
+        }
+
+        public static bool operator !=(cpVect a, cpVect b)
+        {
+            return !(a == b);
+        }
     }
+
 }
 
 #pragma warning restore IDE1006
