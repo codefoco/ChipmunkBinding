0c5a8d3ed244e9d47eaccebd08ff7809a5716aba
Vinicius Jarina <viniciusjarina@gmail.com>
2019-06-30 15:05:37 -0400

    Added `SimpleMotor`

--- src/Constraints/GearJoint.cs
+++ src/Constraints/GearJoint.cs
@@ -1,9 +1,20 @@
 ﻿namespace ChipmunkBinding
 {
+    /// <summary>
+    /// GearJoint keeps the angular velocity ratio of a pair of bodies constant.
+    /// </summary>
     public class GearJoint : Constraint
     {
         public static bool IsGearJoint(Constraint constraint) => NativeMethods.cpConstraintIsGearJoint(constraint.Handle) != 0;
 
+        /// <summary>
+        ///     Keeps the angular velocity ratio of a pair of bodies constant.
+        /// ratio is always measured in absolute terms.It is currently not possible to set the ratio in relation to a third body’s angular velocity.phase is the initial angular offset of the two bodies.
+        /// </summary>
+        /// <param name="bodyA"></param>
+        /// <param name="bodyB"></param>
+        /// <param name="phase"></param>
+        /// <param name="ratio"></param>
         public GearJoint(Body bodyA, Body bodyB, double phase, double ratio):
             base(NativeMethods.cpGearJointNew(bodyA.Handle, bodyB.Handle, phase, ratio))
         {
