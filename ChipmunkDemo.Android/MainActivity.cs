6f73fe9acfe535c034aefed6a0c19bf6e9616624
Vinicius Jarina <viniciusjarina@gmail.com>
2019-07-30 23:46:16 -0400

    Added `ChipmunkDemo.Android`

---
+++ ChipmunkDemo.Android/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Microsoft.Xna.Framework;
+using Android.Views;
+
+namespace ChipmunkDemo.Android
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true, AlwaysRetainTaskState = true)]
+    public class MainActivity : AndroidGameActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            var game = new ChipmunkDemoGame();
+            SetContentView((View)game.Services.GetService(typeof(View)));
+            game.Run();
+        }
+    }
+}
