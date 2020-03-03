94bdb410d7059e104c4c8207cc7f37c413475627
Vinicius Jarina <viniciusjarina@gmail.com>
2020-03-03 08:59:59 -0800

    Add Log to Publish.ps1

--- devops/Publish.ps1
+++ devops/Publish.ps1
@@ -12,6 +12,10 @@ $snupkgFile = $PackageId + '.snupkg'
 & nuget push $nupkgFile -Source https://www.nuget.org/api/v2/package
 
 if (Test-Path -Path $snupkgFile) {
+	Write-Output "Pushing Symbol Package..."
+
 	& nuget push $snupkgFile -Source https://www.nuget.org/api/v2/package
+} else {
+	Write-Output "Skipping Symbol Registration..."
 }
 
