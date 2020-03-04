45d51ba5d7e876ccdb43e11ff644168c6459fbf3
Vinicius Jarina <viniciusjarina@gmail.com>
2020-03-03 23:32:00 -0800

    Fixed symbol nuspec

--- devops/Publish.ps1
+++ devops/Publish.ps1
@@ -7,15 +7,6 @@ if (-Not (Test-Should-Deploy)) {
 }
 
 $nupkgFile  = $PackageId + '.nupkg'
-$snupkgFile = $PackageId + '.snupkg'
 
 & nuget push $nupkgFile -Source https://api.nuget.org/v3/index.json
-
-if (Test-Path -Path $snupkgFile) {
-	Write-Output "Pushing Symbol Package..."
-
-	& nuget push $snupkgFile -Source https://api.nuget.org/v3/index.json
-} else {
-	Write-Output "Skipping Symbol Registration..."
-}
 
