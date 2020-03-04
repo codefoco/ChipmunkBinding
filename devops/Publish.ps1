3373448790a4d7ef1fb26796d32bbf73194358a7
Vinicius Jarina <viniciusjarina@gmail.com>
2020-03-03 20:27:55 -0800

    Use correct source for NuGet

--- devops/Publish.ps1
+++ devops/Publish.ps1
@@ -9,12 +9,12 @@ if (-Not (Test-Should-Deploy)) {
 $nupkgFile  = $PackageId + '.nupkg'
 $snupkgFile = $PackageId + '.snupkg'
 
-& nuget push $nupkgFile -Source https://www.nuget.org/api/v2/package
+& nuget push $nupkgFile -Source https://api.nuget.org/v3/index.json
 
 if (Test-Path -Path $snupkgFile) {
 	Write-Output "Pushing Symbol Package..."
 
-	& nuget push $snupkgFile -Source https://www.nuget.org/api/v2/package
+	& nuget push $snupkgFile -Source https://api.nuget.org/v3/index.json
 } else {
 	Write-Output "Skipping Symbol Registration..."
 }
