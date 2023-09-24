d89aeeefd6d5eb28b3d30a47487a1036bff7af92
Vinicius Jarina <viniciusjarina@gmail.com>
2023-09-24 11:53:50 -0700

    Fix wrong cast

--- src/Shapes/Shape.cs
+++ src/Shapes/Shape.cs
@@ -1,20 +1,20 @@
-﻿// .      ______          __     ____               
-//       / ____/___  ____/ /__  / __/___  _________ 
+﻿// .      ______          __     ____
+//       / ____/___  ____/ /__  / __/___  _________
 //      / /   / __ \/ __  / _ \/ /_/ __ \/ ___/ __ \
 //     / /___/ /_/ / /_/ /  __/ __/ /_/ / /__/ /_/ /
-//     \____/\____/\__, _/\___/_/  \____/\___/\____/ 
-//     
+//     \____/\____/\__, _/\___/_/  \____/\___/\____/
+//
 //     Copyright (c) 2023 Codefoco LTDA - The above copyright notice and this permission notice shall be
 //     included in all copies or substantial portions of the Software.
 //
 //     Redistribution and use in source and binary forms, with or without
 //     modification, are permitted only if explicitly approved by the authors.
 //
-//     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+//     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 //     EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
 //     OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
 //     NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-//     HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+//     HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 //     WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 //     FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 //     OTHER DEALINGS IN THE SOFTWARE.
@@ -233,7 +233,7 @@ namespace ChipmunkBinding
         public int CollisionType
         {
             get => (int)(uint)NativeMethods.cpShapeGetCollisionType(shape);
-            set => NativeMethods.cpShapeSetCollisionType(shape, (UIntPtr)value);
+            set => NativeMethods.cpShapeSetCollisionType(shape, (uint)value);
         }
 
         /// <summary>
